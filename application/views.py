81a4232bac4bb4d12e8c006b6b63eeba23661692
yuyi5187 <kimyy200063@gmail.com>
2024-02-04 19:21:28 +0900

    지원서 생성 고유번호 보내기

--- application/views.py
+++ application/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from  django.core.mail import send_mail, BadHeaderError
+from rest_framework import generics, mixins, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Application
+from .serializers import AppSerializer
+import uuid
+
+class AppCreateListView(generics.ListCreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = AppSerializer
+    
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        student_number = request.data.get('student_number')
+        email = request.data.get('email')
+        field = request.data.get('field')
+        apply_id = uuid.uuid4()
+        
+
+        try:
+            subject = '지원서 고유번호 안내 메일'
+            message = f'{name}님의 지원서 고유번호는 {apply_id} 입니다'
+            from_email = 'sogang@likelion.org'  # 발신 이메일 주소 입력
+            recipient_list = [email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+            
+        except BadHeaderError:
+            return Response({'message': 'Invalid email header. Please try again.'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # 예외가 발생하면 사용자에게 적절한 오류 메시지 반환
+            return Response({'message': f'Error sending email: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # 모델 객체 생성 및 저장
+        application = Application.objects.create(
+            name=name,
+            student_number=student_number,
+            email=email,
+            field=field,
+            apply_id=apply_id
+        )
+
+        serializer = AppSerializer(application)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+# class AppDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Application.objects.all()
+#     serializer_class = AppSerializer
+    
+    
