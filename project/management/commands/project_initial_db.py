cbcfe1feed427c85a589b9e86ce53526b7e6ea93
13155a1 <ppy040@gmail.com>
2024-02-16 21:17:27 +0900

    [FIX] 사진 누락 수정

--- project/management/commands/project_initial_db.py
+++ project/management/commands/project_initial_db.py
@@ -30,7 +30,7 @@ class Command(BaseCommand):
                 'year': 2023,  
                 'team_name': '효를 아십니까',
                 'member_list': '김규빈 김민지 송경호 임정연 정태현',
-                'project_image': '' ,
+                'project_image': 'SILVERCAMPUS' ,
                 'content': '연령에 따른 디지털 공간 격차를 해소하기 위한, 시니어 온라인 강의&커뮤니티 \
                     플랫폼입니다. 타 강의 웹 서비스와의 차별점은 다음과 같습니다. 간단하고 편리한 UI, 음성 안내 서비스, \
                     시니어 맞춤 컨텐츠, 소셜 페이지 ‘청년 광장 등의 서비스를 제공합니다.',
